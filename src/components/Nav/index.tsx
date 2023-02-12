b2413991f963317a9439014ada6496d3959e7254
hanseulhee <3021062@gmail.com>
2023-02-12 20:52:31 +0900

    [#2] apply nav components

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,5 +1,39 @@
+import Link from "next/link";
+import styled from "styled-components";
+import AddIcon from "@mui/icons-material/Add";
+
 function Nav() {
-  return;
+  return (
+    <StyledNavWrapper>
+      <Link href="/Main">
+        <StyledLogo>DontWorry</StyledLogo>
+      </Link>
+      <Link href="/Main/Write">
+        <StyledWritePageBtn />
+      </Link>
+    </StyledNavWrapper>
+  );
 }
 
 export default Nav;
+
+const StyledNavWrapper = styled.nav`
+  position: sticky;
+  top: 0;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  padding: 1rem;
+`;
+
+const StyledLogo = styled.h1`
+  cursor: pointer;
+`;
+
+const StyledWritePageBtn = styled(AddIcon)`
+  cursor: pointer;
+`;
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,11 @@
+import Nav from "src/components/Nav";
+
+function Main() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default Main;
