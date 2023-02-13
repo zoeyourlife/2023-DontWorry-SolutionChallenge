f2f847b7f2983be471d1833a94b9c96950429442
hanseulhee <3021062@gmail.com>
2023-02-13 13:54:58 +0900

    [#10] site title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,6 @@ function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0 viewport-fit=cover"
-        />
-        <meta property="og:title" content="DontWorry" />
 
         {/* google analytics */}
         <script
