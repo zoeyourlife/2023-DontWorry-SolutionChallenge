6bebf2c6947aaa0e93a8fa59d86a52140999b2ec
hanseulhee <3021062@gmail.com>
2023-02-13 13:13:45 +0900

    [#10] apply hotjar

--- src/hooks/useTrackingPage.ts
+++ src/hooks/useTrackingPage.ts
@@ -4,14 +4,17 @@ import * as gtag from "src/libs/analytics/gtag";
 
 const useTrackingPage = () => {
   const router = useRouter();
+
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
       gtag.pageview(url);
     };
-
     router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
     };
   }, [router.events]);
 };
