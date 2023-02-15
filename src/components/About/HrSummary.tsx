b29ef8c5fcaa079bf3c3499798b179ace04d49bb
hanseulhee <3021062@gmail.com>
2023-02-15 16:45:14 +0900

    [#18] apply HrSummary components

---
+++ src/components/About/HrSummary.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+interface IProps {
+  title: string;
+  summary: string;
+}
+
+function HrSummary({ title, summary }: IProps) {
+  return (
+    <StyledIconSummaryWrapper>
+      {title}
+      <StyledHr />
+      <p>{summary}</p>
+    </StyledIconSummaryWrapper>
+  );
+}
+
+export default HrSummary;
+
+const StyledIconSummaryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  width: 7rem;
+  margin-top: 1rem;
+  font-size: 1.1rem;
+  p {
+    opacity: 0.95;
+    font-size: 1rem;
+    margin: 1rem 0rem;
+  }
+`;
+
+const StyledHr = styled.hr`
+  margin-top: 0.2rem;
+  width: 7rem;
+  height: 2px;
+`;
