89f783b0edd8ce21d5fba4544ea4900da924e341
hanseulhee <3021062@gmail.com>
2023-02-15 16:23:50 +0900

    [#18] about page msg

---
+++ src/constants/aboutFourKeyword.ts
@@ -0,0 +1,18 @@
+export const AboutFourKeyword = [
+  {
+    title: "Evidence",
+    summary: "Report photo or what you've been through.",
+  },
+  {
+    title: "See it",
+    summary: "Easily See them in Card Gallery",
+  },
+  {
+    title: "Collect",
+    summary: "Collect information about violence",
+  },
+  {
+    title: "Help",
+    summary: "Ask for help",
+  },
+];
