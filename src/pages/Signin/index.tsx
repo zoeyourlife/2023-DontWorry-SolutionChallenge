c22699aefb447909269bca62712ccfc9fb427b6e
zoeyourlife <201814112@office.skhu.ac.kr>
2023-02-23 22:46:56 +0900

    [#29] setup Signin components
    로그인 컴포넌트 셋업

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,8 +1,66 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { ChangeEvent, useCallback, useState } from "react";
+import { API_BASE_URL } from "src/constants/apiUrl";
+
 function Signin() {
-  return (
-    <>
-      <p>1</p>
-    </>
+  const router = useRouter();
+  const [userId, setUserId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      try {
+        await axios
+          .post(`${API_BASE_URL}/login`, {
+            Id: userId,
+            password: password,
+          })
+          .then((res) => {
+            console.log(res.data);
+            router.push("/Main");
+          });
+        // Id, Pw 에러
+      } catch (err) {
+        console.log(err);
+        alert("Incorrect Info Id or Pw");
+      }
+
+      return (
+        <>
+          <div>
+            <h1>Login</h1>
+            <h3>Report & Report</h3>
+          </div>
+          <form onSubmit={onSubmit}>
+            <div>
+              <input type="text" />
+            </div>
+            <div>
+              <input type="password" />
+            </div>
+            <input type="checkbox" />
+            store? Account
+            <button>Login</button>
+          </form>
+
+          <div>
+            <h3>No account?</h3>
+            <button>create account</button>
+          </div>
+        </>
+      );
+    },
+    [userId, password, router]
   );
 }
 
