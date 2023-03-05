80a8c7a1abcea5fffab0e47b67ecc62a6133b0fb
hanseulhee <3021062@gmail.com>
2023-03-05 15:36:04 +0900

    [#32] add explain period

--- src/constants/writeExplain.ts
+++ src/constants/writeExplain.ts
@@ -6,21 +6,21 @@ interface IType {
 export const writeExplain: IType[] = [
   {
     title: "Title",
-    subTitle: "Please enter the title of the article",
+    subTitle: "Please enter the title of the article.",
   },
   {
     title: "Date",
     subTitle:
-      "Please enter the date the incident occurred. If you can't remember the exact date, you don't have to enter it",
+      "Please enter the date the incident occurred. If you can't remember the exact date, you don't have to enter it.",
   },
   {
     title: "Tag",
     subTitle:
-      "Who gives you a hard time? This is for sorting, so you don't have to type it",
+      "Who gives you a hard time? This is for sorting, so you don't have to type it.",
   },
   {
     title: "Summary",
-    subTitle: "Share your memories of the day here",
+    subTitle: "Share your memories of the day here.",
   },
   {
     title: "Location",
@@ -29,10 +29,10 @@ export const writeExplain: IType[] = [
   {
     title: "Images",
     subTitle:
-      "If you have a picture related to it, you can post it. To be a more reliable source, dontworry gets the location of the image",
+      "If you have a picture related to it, you can post it. To be a more reliable source, dontworry gets the location of the image.",
   },
   {
     title: "Video",
-    subTitle: "If you have a related video, you can post it",
+    subTitle: "If you have a related video, you can post it.",
   },
 ];
