3448b7864033605f18308da942546b3374ab243c
hanseulhee <3021062@gmail.com>
2023-03-07 18:32:06 +0900

    [#59] apply detail page

--- src/pages/Main/Detail/before.tsx
+++ src/pages/Main/Detail/before.tsx
@@ -1,9 +1,8 @@
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useRef } from "react";
-import TodayDate from "src/components/Common/Date/TodayDate";
 import Tag from "src/components/Common/Tag";
-import ReportSubTitle from "src/components/Common/Text/ReportSubTitle";
 import Nav from "src/components/Nav";
 import BottomNav from "src/components/Nav/BottomNav";
 import MakePdf from "src/components/Pdf/MakePdf";
@@ -35,38 +34,28 @@ function Detail() {
     <>
       <Nav />
       <StyledWrapper ref={colorChangeRef} className="pdfWrapper">
+        <StyledTitle>글 제목</StyledTitle>
+        <StyledDate>날짜</StyledDate>
+        <StyledTagWrapper>
+          <Tag category="DontWorry" />
+        </StyledTagWrapper>
         <StyledContentWrapper>
-          <StyledTagWrapper>
-            <Tag category="DontWorry" />
-          </StyledTagWrapper>
-          <StyledTitle>An incident to report</StyledTitle>
-          <ReportSubTitle number={1} title="When" />
-          <StyledContent>글 작성 날짜</StyledContent>
-          <ReportSubTitle number={2} title="The day of the incident" />
-          <StyledContent>사건 발생일</StyledContent>
-          <ReportSubTitle
-            number={3}
-            title="The place where the incident occurred"
-          />
-          <StyledContent>발생 위치</StyledContent>
-          <ReportSubTitle number={4} title="Incident details" />
-          <StyledContent>사건 내용</StyledContent>
-          <ReportSubTitle number={5} title="Attached photo" />
-          <StyledImgWrapper>
-            <StyledImg>
-              <Image
-                src="/images/food.jpg"
-                alt="cardImg"
-                layout="fill"
-                sizes="13.5rem"
-                quality={100}
-              />
-            </StyledImg>
-          </StyledImgWrapper>
-          <StyledTodayDateWrapper>
-            <TodayDate />
-          </StyledTodayDateWrapper>
+          <StyledContent>글내용</StyledContent>
         </StyledContentWrapper>
+        <StyledLocationWrapper>
+          <StyledLocationIcon /> <StyledLocation>장소 명</StyledLocation>
+        </StyledLocationWrapper>
+        <StyledImgWrapper>
+          <StyledImg>
+            <Image
+              src="/images/food.jpg"
+              alt="cardImg"
+              layout="fill"
+              sizes="13.5rem"
+              quality={100}
+            />
+          </StyledImg>
+        </StyledImgWrapper>
       </StyledWrapper>
       <StyledBtnWrapper>
         <StyledBtn onClick={onSaveBtnClick}>save as pdf</StyledBtn>
@@ -79,18 +68,42 @@ function Detail() {
 export default Detail;
 
 const StyledWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
   width: 100%;
   min-height: 100%;
   padding: 1rem 1rem;
 `;
 
-const StyledTitle = styled.h1`
-  text-align: center;
-  font-size: 1.43rem;
+const StyledTitle = styled.span`
   font-weight: ${({ theme }) => theme.fontWeight.bold};
-  margin: 1rem 0;
+  font-size: 1.5rem;
+`;
+
+const StyledDate = styled.p`
+  opacity: 0.5;
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+  font-size: 0.8rem;
+  margin: 0.5rem 0;
+`;
+
+const StyledLocationWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: fit-content;
+  cursor: pointer;
+`;
+
+const StyledLocationIcon = styled(LocationOnIcon)`
+  color: ${({ theme }) => theme.color.grey};
+  width: 1.2rem;
+  margin-left: 0.2rem;
+`;
+
+const StyledLocation = styled.span`
+  opacity: 0.5;
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+  font-size: 0.8rem;
+  margin-left: 0.2rem;
 `;
 
 const StyledTagWrapper = styled.div`
@@ -99,15 +112,15 @@ const StyledTagWrapper = styled.div`
   width: 100%;
 `;
 
-const StyledContent = styled.span`
-  font-weight: ${({ theme }) => theme.fontWeight.light};
-  font-size: 0.9rem;
-  margin: 0.5rem 0;
+const StyledContentWrapper = styled.div`
+  height: auto;
+  margin: 2rem 0;
 `;
 
-const StyledContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+const StyledContent = styled.p`
+  line-height: 1.3rem;
+  font-weight: ${({ theme }) => theme.fontWeight.light};
+  font-size: 1rem;
 `;
 
 const StyledImgWrapper = styled.div`
@@ -127,10 +140,6 @@ const StyledImg = styled.div`
   margin: 0.5rem 0rem 0.5rem 0;
   border-radius: ${({ theme }) => theme.borderRadius.imgCard};
   overflow: hidden;
-`;
-
-const StyledTodayDateWrapper = styled.div`
-  margin: 2.5rem 0;
 `;
 
 const StyledBtnWrapper = styled.div`
