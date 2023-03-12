c230eec608eb743a4990217a039694a53bafd864
hanseulhee <3021062@gmail.com>
2023-03-12 20:02:16 +0900

    [#9] type definition

--- src/constants/appClient.ts
+++ src/constants/appClient.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_BASED_URL } from "./apiUrl";
+import { API_BASED_URL } from "src/constants/apiUrl";
 
 // axios의 .create() 내장 함수 이용해서 통신할 때 필요한 쿼리나, 헤더, 바디 등등 여러 데이터 함께 보낼 수 있음
 // 추후 모듈화 과정 때 사용.
---
+++ src/remotes/upload.d.ts
@@ -0,0 +1,11 @@
+/*eslint no-undef: "off"*/
+
+interface IPostWriteData {
+  title: string;
+  category: string;
+  mainText: string;
+  userId: string;
+  location: string;
+  imagesFiles: IPhoto[];
+  incidentDate: string;
+}
