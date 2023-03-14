f352c086ff37445e37eb7367580b78cb44a0bb70
zoeyourlife <201814112@office.skhu.ac.kr>
2023-03-15 01:34:01 +0900

    [#57] select option component
    feat: select box component

--- src/components/SelectCountryT/index.tsx
+++ src/components/SelectCountryT/index.tsx
@@ -14,10 +14,10 @@ function SelectCountryT() {
         <option selected disabled>
           Choose Country ...
         </option>
-        <option value="US">United States</option>
-        <option value="KR">Korea</option>
-        <option value="JP">Japan</option>
-        <option value="CN">China</option>
+        <option value="US">🇺🇸 | United States</option>
+        <option value="KR">🇰🇷 | Korea</option>
+        <option value="JP">🇯🇵 | Japan</option>
+        <option value="CN">🇨🇳 | China</option>
       </StyledSelect>
       {selectedCountry && <h2>{selectedCountry}</h2>}
     </StyledSelectDiv>
@@ -34,10 +34,33 @@ const StyledSelectDiv = styled.div`
 `;
 
 const StyledSelect = styled.select`
-  width: 9rem;
+  width: 10rem;
   padding: 0.5rem;
-  border: 0.0425rem solid #999;
-  border-radius: 5px;
-  background-color: #1f1f1f;
-  color: white;
+  border: 0.0425rem solid ${({ theme }) => theme.color.grey100};
+  border-radius: ${({ theme }) => theme.borderRadius.imgCard};
+  background-color: ${({ theme }) => theme.color.background};
+  color: ${({ theme }) => theme.color.white};
+  cursor: pointer;
+
+  :hover {
+    border-color: ${({ theme }) => theme.color.blueGreen};
+    transition: all 0.5s;
+  }
+
+  :focus {
+    border-color: #aaa;
+    box-shadow: 0 0 1px 1px rgba(151, 200, 252, 0.4);
+    color: white;
+    outline: none;
+  }
+
+  > option[value="US"] {
+    background-image: url("/images/america.png");
+    background-repeat: no-repeat;
+  }
+
+  option:checked {
+    background-color: ${({ theme }) => theme.color.grey100};
+    color: ${({ theme }) => theme.color.white};
+  }
 `;
