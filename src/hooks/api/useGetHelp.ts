7e1d400fa6d62dc221763739acd9b963d97cb644
hanseulhee <3021062@gmail.com>
2023-03-17 22:00:42 +0900

    [#86] each country help api

--- src/hooks/api/useGetHelp.ts
+++ src/hooks/api/useGetHelp.ts
@@ -3,27 +3,24 @@ import { instance } from "src/constants/appClient";
 import { IGetHelpData, IGetHelpDataItem } from "src/remotes/help";
 
 interface UseGetHelpByCountryProps {
-  country: string | string[] | undefined;
+  countryName: string | string[] | undefined;
 }
 
-function useGetHelp({ country }: UseGetHelpByCountryProps) {
+function useGetHelp({ countryName }: UseGetHelpByCountryProps) {
   const [helpData, setHelpData] = useState<IGetHelpDataItem[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  async function getHelpData() {
-    // async function getHelpData(name: string) {
+
+  async function getHelpData(name: string) {
     setIsLoading(true);
-    // const response = await instance.get<{}, IGetHelpData>(`/help/${name}`);
-    const response = await instance.get<{}, IGetHelpData>(`/help/japan`);
+    const response = await instance.get<{}, IGetHelpData>(`/help/${name}`);
+
     setHelpData(response.data);
     setIsLoading(false);
   }
 
   useEffect(() => {
-    getHelpData();
-  }, []);
-  // useEffect(() => {
-  //   if (typeof country === "string") getHelpData(country);
-  // }, [country, setHelpData]);
+    if (typeof countryName === "string") getHelpData(countryName);
+  }, [countryName, setHelpData]);
 
   return {
     helpData,
