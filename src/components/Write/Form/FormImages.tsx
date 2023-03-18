5da3f16a8e39750e55194d101de5338b042ed363
hanseulhee <3021062@gmail.com>
2023-03-18 23:16:02 +0900

    [#9] type definition

--- src/components/Write/Form/FormImages.tsx
+++ src/components/Write/Form/FormImages.tsx
@@ -1,20 +1,15 @@
-import { ChangeEvent } from "react";
 import ImageUpload from "src/components/Write/ImageUpload";
 import Title from "src/components/Write/Title";
 import { writeExplain } from "src/constants/writeExplain";
 
-interface IProps {
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-}
-
-function FormImages({ onChange }: IProps) {
+function FormImages() {
   return (
     <>
       <Title
         title={writeExplain[5].title}
         subTitle={writeExplain[5].subTitle}
       />
-      <ImageUpload onChange={onChange} />
+      <ImageUpload />
     </>
   );
 }
--- src/remotes/upload.d.ts
+++ src/remotes/upload.d.ts
@@ -2,10 +2,16 @@ import { IPhoto } from "src/remotes/photo";
 
 export interface IPostWriteData {
   title: string;
-  category: string | string[];
+  category: IPostCategory[];
   mainText: string;
   userId: string;
   location: string;
   imagesFiles: IPhoto[];
   incidentDate: string;
+}
+
+interface IPostCategory {
+  id: number;
+  categoryName: string;
+  posts: null;
 }
