658347eda570feb683fa3237277529bed0806e95
jaegwans <skhu201811140@gmail.com>
2023-03-19 22:26:30 +0900

    [#74] fix ImageFolder,ImageFolderDetail api call

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: [''],
-    },
-    compiler: {
-        styledComponents: true,
-    },
+  reactStrictMode: true,
+  images: {
+    domains: ["dontworry.duckdns.org", "storage.googleapis.com"],
+  },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
