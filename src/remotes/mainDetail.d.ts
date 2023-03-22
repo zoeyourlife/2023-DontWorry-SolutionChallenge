0a0c1d03be3e609a684e3d67ac18c0bd0eb805d8
hanseulhee <3021062@gmail.com>
2023-03-23 00:45:59 +0900

    [#88] detail page by id

--- src/remotes/mainDetail.d.ts
+++ src/remotes/mainDetail.d.ts
@@ -2,6 +2,7 @@ export interface IGetMainDetailDataItem {
   id: number;
   title: string;
   incidentDate: string;
+  createdDate: string;
   location: string;
   storeFileName: string[];
   mainText: string;
