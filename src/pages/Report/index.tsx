baf01a135b58849822144aab8626b102ebeec5aa
jaegwans <skhu201811140@gmail.com>
2023-04-03 17:00:47 +0900

    [#126] fix back btn

--- src/pages/Report/index.tsx
+++ src/pages/Report/index.tsx
@@ -5,12 +5,15 @@ import BottomNav from "src/components/Nav/BottomNav";
 import Search from "src/components/Search";
 import Timeline from "src/components/Timeline";
 import useGetMain from "src/hooks/api/useGetMain";
-import {IGetMainDataItem } from "src/remotes/main";
+import { IGetMainDataItem } from "src/remotes/main";
 import styled from "styled-components";
 
 function Report() {
   const { mainData, isLoading } = useGetMain();
   const [searchData, setSearchData] = useState<IGetMainDataItem[]>([]);
+  function onClickBack() {
+    setSearchData([]);
+  }
 
   if (isLoading) {
     return <Loading />;
@@ -21,6 +24,7 @@ function Report() {
         <Nav />
         <StyledWrapper>
           <Search />
+          <StyledBack onClick={onClickBack}>Back</StyledBack>
           {searchData.map((data, i) => (
             <Timeline
               key={i}
@@ -62,6 +66,15 @@ function Report() {
 
 export default Report;
 
+const StyledBack = styled.button`
+  background-color: ${({ theme }) => theme.color.borderGrey};
+  width: 100%;
+  height: 3.25rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: ${({ theme }) => theme.borderRadius.button};
+`;
 const StyledWrapper = styled.div`
   position: relative;
   width: 100%;
